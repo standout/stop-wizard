80bba51d7fdc9960c9ceb1bbc7c9e2f3b645d60f
Vera <vera@Veras-MacBook-Pro.local>
2017-04-20 08:36:57 +0200

    Update code

--- game/wizard.js
+++ game/wizard.js
@@ -3,10 +3,7 @@ function startWizardGame () {
   fullscreenWizardSection.classList.remove('hidden')
   // here
   alertDialog('Niko: ${player.name} are you here?')
-  alertDialog(`${player.name}: yes? where are we?`)
-  alertDialog("Niko: we are here, in the dark forest")
-  alertDialog(`${player.name}: where!?`)
-  alertDialog("Niko: in the dark forest, where the wizard lives")
+ 
 }
 
 function closeWizardGame () {
